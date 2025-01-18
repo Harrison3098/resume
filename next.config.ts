092e0246a6669d117dfbc50ac2a4db0fddc80e9b
Harrison Joel <112457263+Harrison3098@users.noreply.github.com>
2025-01-19 01:09:28 +0800

    chore: 静态打包命令

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  assetPrefix: "/resume/", // 替换为你的 GitHub 仓库名称
+  images: {
+    unoptimized: true, // 如果你使用了图片，GitHub Pages 上的图片优化会导致一些问题
+  },
 };
 
 export default nextConfig;
